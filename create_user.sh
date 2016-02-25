ddeaf901f77bf43cc67a72f2221b3dde7d8c30e1
XiongHui Guo <gz_shines@msn.com>
2016-02-25 17:49:48 +0800

    Small modifications.
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

---
+++ create_user.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# For Check_Is_Root_Account(), Echo_Color()
+. include/echo_color.sh
+. include/common.sh
+. include/network.sh
+. include/path.sh
+
+# Check whether the logon user is a root account?
+Check_Is_Root_Account
+
+cur_dir=$(pwd)
+
+. version.sh
+. config.sh
+
+# Process ${MY_CLUSTER_DIR}_DIR
+if [[ -z "${MY_CLUSTER_DIR}" || "${MY_CLUSTER_DIR}" = "../" ]]; then
+    MY_CLUSTER_DIR="${cur_dir}/../"
+else
+    if [ "${MY_CLUSTER_DIR}" = "./" ]; then
+        MY_CLUSTER_DIR="${cur_dir}"
+    fi
+fi
+MY_CLUSTER_DIR=$(Check_PathName ${MY_CLUSTER_DIR})
+
+# Process $CEPH_LOCAL_HOSTNAME
+if [ -z "${CEPH_LOCAL_HOSTNAME}" ]; then
+    CEPH_LOCAL_HOSTNAME=$(Get_Local_HostName)
+fi
+
+# Process $CEPH_LOCAL_HOSTIP
+if [ -z "${CEPH_LOCAL_HOSTIP}" ]; then
+    CEPH_LOCAL_HOSTIP=$(Get_Local_HostIP)
+fi
+
+# Process CEPH_RADOSGW_HOST
+if [ -z "${CEPH_RADOSGW_HOST}" ]; then
+    CEPH_RADOSGW_HOST=$(Get_Local_HostName)
+fi
+
+# Process CIVETWEB_PORT
+if [ -z "${CIVETWEB_PORT}" ]; then
+    CIVETWEB_PORT="8000"
+fi
+
+. include/linux_version.sh
+
+Get_Linux_Dist_Name
+
+if [ "${DISTRO}" = "unknow" ]; then
+    Echo_Red "Error: Unable to get the Linux distribution name, or do NOT support the current distribution."
+    exit 1
+fi
+
+. include/random.sh
+
+function Display_Welcome()
+{
+    clear
+    echo ""
+    echo "+------------------------------------------------------------------------+"
+    echo "|                                                                        |"
+    Echo_Cyan_Ex "|" "               Ceph RGW S3 Shell Script for Linux Server                " "|"
+    echo "|                                                                        |"
+    echo "|                           Version: ${CEPH_RGW_S3_Version}                               |"
+    echo "|                           Host OS: ${DISTRO}                            "
+    echo "|                                                                        |"
+    echo "|                         Author by: shines77                            |"
+    echo "|                     Last Modified: ${CEPH_RGW_S3_LastModified}                          |"
+    echo "|                                                                        |"
+    echo "+------------------------------------------------------------------------+"
+    echo "|         A tool to auto-compile & install Ceph RGW S3 on Linux          |"
+    echo "+------------------------------------------------------------------------+"
+    echo "|     For more information please visit http://cephrgws3.skyinno.com     |"
+    echo "+------------------------------------------------------------------------+"
+    echo ""
+}
+
+Display_Welcome
+
+mkdir -p ${MY_CLUSTER_DIR}
+cd ${MY_CLUSTER_DIR}
+
+radosgw-admin user create --uid=s3_test --display-name="S3 test user" --email=admin@example.com
+
